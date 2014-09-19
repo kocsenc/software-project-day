b4213aa48f086687f0500e2b27cf3719b506d078
Kocsen Chuung <kocsenc@gmail.com>
2014-09-19 13:48:50 -0400

    woolie constructor

--- app/src/Woolie.java
+++ app/src/Woolie.java
@@ -1,6 +1,18 @@
 /**
  * Created by kocsenc on 9/19/14.
  */
-public class Woolie {
-    
+
+public class Woolie extends Thread {
+    private String name;
+    private int speed;
+    private String destinationCity;
+
+    Woolie(String name, int speed, String destinationCity){
+        this.name = name;
+        this.speed = speed;
+        this.destinationCity = destinationCity;
+    }
+
+
 }
+
