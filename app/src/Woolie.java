adf52040260f0afa8ab069648597b932859ab509
cman131 <cgw5994@rit.edu>
2014-09-19 13:52:30 -0400

    implemented run

--- app/src/Woolie.java
+++ app/src/Woolie.java
@@ -1,6 +1,17 @@
 /**
  * Created by kocsenc on 9/19/14.
  */
-public class Woolie {
-    
+public class Woolie { 
+	
+    public void run(){
+    	int start = 0;
+    	System.out.println(this.name + " has arrived at the bridge.");
+    	System.out.println(this.name + " is starting to cross.");
+    	while(start!=this.speed){
+    		start+=1;
+    		System.out.println("\t"+this.name + " "+start+" seconds.");
+    		Thread.sleep(1000);
+    	}
+    	System.out.println(this.name + " leaves at "+this.destinationCity+".");
+    }
 }
