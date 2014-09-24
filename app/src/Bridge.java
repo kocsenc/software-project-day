8393b321308984f2f0acdd2debc36b7605a9e609
Kocsen Chuung <kocsenc@gmail.com>
2014-09-23 23:18:23 -0400

    - cleanup made bridge non-static

--- app/src/Bridge.java
+++ app/src/Bridge.java
@@ -1,19 +1,53 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
- * Created by kocsenc on 9/23/14.
+ * Created by Kocsen & Conor on 9/19/14.
+ * Woolie crossing the bridge
  */
+
+
 public class Bridge {
-    private static boolean woolieOnBridge = false;
+    private final int MAX_CAPACITY;
 
-    public static synchronized boolean enterBridge() {
-        if (Bridge.woolieOnBridge) {
-            return false;
-        } else {
-            Bridge.woolieOnBridge = true;
-            return true;
+    private ArrayList<Woolie> wooliesOnBridge;
+    private LinkedList<Woolie> woolieQueue;
+
+    /**
+     * @param capacity - number of woolies that a single bridge can hold
+     */
+    Bridge(int capacity) {
+        this.MAX_CAPACITY = capacity;
+        this.wooliesOnBridge = new ArrayList<>();
+        this.woolieQueue = new LinkedList<>();
+    }
+
+    /**
+     * @param woolie - the woolie entering
+     * @return True or False depending if woolie on bridge or not
+     */
+    public synchronized boolean enterBridge(Woolie woolie) throws InterruptedException {
+        woolieQueue.addLast(woolie);
+
+        // While bridge is full & not first, tell woolie to wait
+        while (wooliesOnBridge.size() == MAX_CAPACITY && !woolieQueue.peek().equals(woolie)) {
+            woolie.wait();
         }
+
+        // pop it from the stack and woolie to bridge
+        woolieQueue.remove();
+        wooliesOnBridge.add(woolie);
+        woolie.notify();
+        return true;
     }
 
-    public static void leaveBridge() {
-        Bridge.woolieOnBridge = false;
+    /**
+     * Removes the woolie from the bridge array
+     * @param woolie - the woolie leaving
+     */
+    public void leaveBridge(Woolie woolie) {
+        wooliesOnBridge.remove(woolie);
+        notify();
     }
+
 }
--- app/src/Woolie.java
+++ app/src/Woolie.java
@@ -9,25 +9,24 @@ public class Woolie extends Thread {
     private String name;
     private int speed;
     private String destinationCity;
+    private Bridge bridge;
 
     Woolie(String name, int speed, String destinationCity, Bridge bridge) {
         this.name = name;
         this.speed = speed;
         this.destinationCity = destinationCity;
+        this.bridge = bridge;
     }
 
     public void run() {
         int start = 0;
         System.out.println(this.name + " has arrived at the bridge.");
-        boolean bool = false;
-        while(!bool){
-        	try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        	bool = Bridge.enterBridge();
+        try {
+            bridge.enterBridge(this);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
         System.out.println(this.name + " is starting to cross.");
         while (start != this.speed) {
             start += 1;
@@ -39,15 +38,16 @@ public class Woolie extends Thread {
             }
         }
         System.out.println(this.name + " leaves at " + this.destinationCity + ".");
-        Bridge.leaveBridge();
+        bridge.leaveBridge(this);
     }
-    
-    public static void main(String[] args){
-    	ArrayList<Woolie> woolies = new ArrayList<Woolie>();
-    	for(int i=0; i<5; i++){
-    		woolies.add(new Woolie("Woolie-"+i, 10-i, "DatCity", new Bridge()));
-    		woolies.get(i).start();
-    	}
+
+    public static void main(String[] args) {
+        ArrayList<Woolie> woolies = new ArrayList<Woolie>();
+        Bridge b = new Bridge(3);
+        for (int i = 0; i < 5; i++) {
+            woolies.add(new Woolie("Woolie-" + i, 10 - i, "DatCity", b));
+            woolies.get(i).start();
+        }
     }
 
 }
