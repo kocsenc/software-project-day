695c3dea2c163cab5d62ab56d44123c14131b309
kocsen <kocsenc@gmail.com>
2014-10-17 13:51:31 -0400

    Main Class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main Class
+ * Entry point of the software project day program
+ */
+
+public class Main {
+
+    public static void main(String[] args){
+        // TODO Stub fillout
+    }
+}
