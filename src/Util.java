db1e85975b3146c8c1084e39c771df6286066f4e
John Deeney <JohnCDeeneY@gmail.com>
2014-10-20 14:34:35 -0400

    Added function to Util
    timeToString() takes a long and returns the time of day it represents.

--- src/Util.java
+++ src/Util.java
@@ -23,6 +23,37 @@ public class Util {
         // so add 1 to make it inclusive
         return rand.nextInt((max - min) + 1) + min;
     }
+    
+    
+    /**
+     * Return the string value of a time, given how many milliseconds it is into the day (e.g. 9:52 A.M.)
+     * 
+     * @param time Time of day, represented in miliseconds from start of day.
+     * @return
+     */
+    public static String timeToString(long time) {
+    	String tooMuchGoodStuff; //AM or PM
+    	String hourString;
+    	String minuteString;
+    	
+    	long hour = (time / 600) + 8;    	
+    	if (hour > 11) {
+    		tooMuchGoodStuff = "P.M.";
+    	}
+    	else {
+    		tooMuchGoodStuff = "A.M.";
+    	}
+    	if (hour > 12) {
+    		hour -= 12;
+    	}
+
+    	hourString = Long.toString(hour);
+    	
+    	int minute = (int)(time % 600) / 10;
+    	minuteString = String.format("%02d", minute);
+    	
+    	return hourString + ":" + minuteString + " " + tooMuchGoodStuff;
+    }
 
 
 }
