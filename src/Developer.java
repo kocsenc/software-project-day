74ade7f7848f4c9f848af7e5ad60a6c007012801
kocsen <kocsenc@gmail.com>
2014-10-22 13:51:28 -0400

    Developer.java - Added arrival - Integrate with TeamLead to notify of arrival (using conditions) - Start implementing asking questions TeamLead.java - Connections with Dev when notifying of arribal - stubbed out procedure for asking a question

--- src/Developer.java
+++ src/Developer.java
@@ -1,9 +1,62 @@
 /**
  * Developer Class
- *
+ * <p/>
  * Work on: Kocsen
  */
 
-public class Developer {
+public class Developer extends Thread {
 
+    public long entered; // Time entered
+    public boolean arrived = false;
+    public boolean askedQuestion = false;
+    public boolean askingQuestion = false;
+    public String name;
+    private Firm firm;
+    private TeamLead teamLead;
+
+
+
+    public Developer(String name, Firm firm, TeamLead teamLead) {
+        this.name = name;
+        this.firm = firm;
+        this.teamLead = teamLead;
+        arrived = false;
+        askingQuestion = false;
+
+    }
+
+    public void run() {
+        try {
+            // Wait for up to 30 mins.
+            Thread.sleep(Util.randomInBetween(0, FirmTime.HALF_HOUR.ms()));
+            // Arrive
+            this.entered = firm.getTime();
+            this.arrived = true;
+            System.out.printf("Developer %s has arrived to the workplace at %d%n", name, entered);
+
+
+            // Randomly asking a question
+            this.askQuestion();
+
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void askQuestion() {
+        if(!askedQuestion){
+            this.teamLead.askedQuestion();
+        }
+    }
+
+    /**
+     * Getter for whether the dev has arrived in the morning
+     *
+     * @return true if arrived, false otherwise
+     */
+    public boolean getArrived() {
+        return this.arrived;
+    }
 }
--- src/TeamLead.java
+++ src/TeamLead.java
@@ -18,7 +18,7 @@ public class TeamLead extends Developer {
     private Condition waitForDevs = new Condition(){
     	private boolean isMet(){
 			for(Developer dev : developers){
-				if(dev.entered==null){
+				if(!dev.getArrived()){
 					return false;
 				}
 			}
@@ -31,7 +31,8 @@ public class TeamLead extends Developer {
 		}
 	};
 
-    public TeamLead(SoftwareProjectManager manager, Firm firm, List<Developer> developers) {
+    public TeamLead(String name, SoftwareProjectManager manager, Firm firm, List<Developer> developers) {
+        super(name, firm, this); //TODO: this won't work out
         this.manager = manager;
         this.firm = firm;
         this.developers = (ArrayList<Developer>) developers;
@@ -48,6 +49,30 @@ public class TeamLead extends Developer {
     
     public void unlock(){
     	locked = false;
+    }
+
+    /**
+     * Developer calls this method when asking a question
+     */
+    public void askedQuestion(){
+        // 50% chance of coming up with the answer
+        boolean canIAnswer = Util.randomInBetween(0,1) == 1;
+        try {
+            // 10 minute wait
+            Thread.sleep(FirmTime.MINUTE.ms() * 10);
+            // See if we need to talk to the SPM incase we don't know the answer
+            if (!canIAnswer){
+                this.askQuestion();
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void askQuestion(){
+        // TODO: This is what runs when we need to ask a question to the SPM
     }
 
     /**
