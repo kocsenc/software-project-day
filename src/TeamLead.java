866b3b2f5507101daa0799dac54247c6f4c12e25
kocsen <kocsenc@gmail.com>
2014-10-24 13:21:53 -0400

    -team lead add

--- src/TeamLead.java
+++ src/TeamLead.java
@@ -1,81 +1,83 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Team Lead Class
- *
+ * <p/>
  * Work on: Conor
  */
 
-public class TeamLead extends Developer {
+public class TeamLead {
     private final SoftwareProjectManager manager;
     private final ArrayList<Developer> developers;
-    private final Firm firm;
-    private boolean locked = false;
-    
     // Waits
-    private Condition waitForDevs = new Condition(){
-    	private boolean isMet(){
-			for(Developer dev : developers){
-				if(!dev.getArrived()){
-					return false;
-				}
-			}
-			return true;
-		}
-	};
-    private Condition waitForUnlock = new Condition(){
-    	private boolean isMet(){
-			return !locked;
-		}
-	};
+    private Condition waitForDevs = new Condition() {
+        private boolean isMet() {
+            for (Developer dev : developers) {
+                if (!dev.getArrived()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    };
+    public long entered;
+    private Firm firm;
+    private boolean locked = false;
+    private Condition waitForUnlock = new Condition() {
+        private boolean isMet() {
+            return !locked;
+        }
+    };
 
     public TeamLead(String name, SoftwareProjectManager manager, Firm firm, List<Developer> developers) {
-        super(name, firm, this); //TODO: this won't work out
         this.manager = manager;
         this.firm = firm;
         this.developers = (ArrayList<Developer>) developers;
     }
-    
-    public void run(){
-    	this.entered = firm.getTime();
-    	manager.arrives(this);
-    	locked = true;
-    	waitForUnlock.waitUntilMet(100);
-    	waitForDevs.waitUntilMet(100);
-    	for(Developer dev : developers){
-            // TODO: Merge notes, we cant access varaibles, we need getters & setters.
-    		dev.locked = true;
-    	}
-    	this.locked = true;
-    	try {
-			Thread.sleep(1500);
-		} catch (InterruptedException e) {
-		}
-    	for(Developer dev : developers){
-    		dev.locked = false;
-    	}
-    	this.locked = false;
-    	
-    	
+
+    private void setFirm(Firm firm) {
+        this.firm = firm;
     }
-    
-    public void unlock(){
-    	locked = false;
+
+    public void run() {
+        this.entered = firm.getTime();
+        manager.arrives(this);
+        locked = true;
+        waitForUnlock.waitUntilMet(100);
+        waitForDevs.waitUntilMet(100);
+        for (Developer dev : developers) {
+            // TODO: Merge notes, we cant access varaibles, we need getters & setters.
+            dev.locked = true;
+        }
+        this.locked = true;
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+        }
+        for (Developer dev : developers) {
+            dev.locked = false;
+        }
+        this.locked = false;
+
+
+    }
+
+    public void unlock() {
+        locked = false;
     }
 
     /**
      * Developer calls this method when asking a question
      */
-    public void askedQuestion(){
+    public void askedQuestion() {
         // 50% chance of coming up with the answer
-        boolean canIAnswer = Util.randomInBetween(0,1) == 1;
+        boolean canIAnswer = Util.randomInBetween(0, 1) == 1;
         try {
             // 10 minute wait
             Thread.sleep(FirmTime.MINUTE.ms() * 10);
             // See if we need to talk to the SPM incase we don't know the answer
-            if (!canIAnswer){
+            if (!canIAnswer) {
                 this.askQuestion();
             }
 
@@ -85,15 +87,16 @@ public class TeamLead extends Developer {
 
     }
 
-    public void askQuestion(){
+    public void askQuestion() {
         // TODO: This is what runs when we need to ask a question to the SPM
     }
 
     /**
      * Gets list of developers under this team lead.
+     *
      * @return list of developers under this team lead.
      */
-	public ArrayList<Developer> getDevelopers() {
-		return developers;
-	}
+    public ArrayList<Developer> getDevelopers() {
+        return developers;
+    }
 }
