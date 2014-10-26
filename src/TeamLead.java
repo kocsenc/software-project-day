118f9eb95f6619ad364968984cc8729c00a19fce
kocsen <kocsenc@gmail.com>
2014-10-26 14:12:03 -0400

    - Team Lead reformat & Extending thread

--- src/TeamLead.java
+++ src/TeamLead.java
@@ -3,30 +3,29 @@ import java.util.List;
 
 /**
  * Team Lead Class
- *
+ * <p/>
  * Work on: Conor
  */
 
-public class TeamLead {
-	private static int leads = 0;
+public class TeamLead extends Thread {
+    private static int leads = 0;
     private final SoftwareProjectManager manager;
     private final ArrayList<Developer> developers;
-    private int devArrived = 0;
     private final int id;
     public long entered;
-    private Firm firm;
-    private boolean locked = false;
-    
+    private int devArrived = 0;
     // Waits
     private Condition waitForDevs = new Condition() {
         @SuppressWarnings("unused")
-		private boolean isMet() {
-            return devArrived==3;
+        private boolean isMet() {
+            return devArrived == 3;
         }
     };
+    private Firm firm;
+    private boolean locked = false;
     private Condition waitForUnlock = new Condition() {
         @SuppressWarnings("unused")
-		private boolean isMet() {
+        private boolean isMet() {
             return !locked;
         }
     };
@@ -36,69 +35,70 @@ public class TeamLead {
         this.firm = firm;
         this.developers = (ArrayList<Developer>) developers;
         this.id = TeamLead.leads;
-        TeamLead.leads+=1;
+        TeamLead.leads += 1;
     }
 
     public synchronized void setFirm(Firm firm) {
         this.firm = firm;
     }
-    
-    public synchronized void knock(){
-    	try {
-			Thread.currentThread().wait();
-		} catch (InterruptedException e) {
-		}
-		this.devArrived+=1;
+
+    public synchronized void knock() {
+        try {
+            Thread.currentThread().wait();
+        } catch (InterruptedException e) {
+        }
+        this.devArrived += 1;
     }
 
     public void run() {
-    	try {
-			Thread.sleep(Util.randomInBetween(0, FirmTime.HALF_HOUR.ms()));
-		} catch (InterruptedException e1) {
-		}
+        try {
+            Thread.sleep(Util.randomInBetween(0, FirmTime.HALF_HOUR.ms()));
+        } catch (InterruptedException e1) {
+        }
         this.entered = firm.getTime();
-        System.out.println(firm.getTime()+": TeamLead #"+this.id+" arrives.");
+        System.out.println(firm.getTime() + ": TeamLead #" + this.id + " arrives.");
         manager.knock();
-        
+
         waitForDevs.waitUntilMet(100);
         firm.attemptJoin();
         try {
-            Thread.sleep(15*FirmTime.MINUTE.ms());
+            Thread.sleep(15 * FirmTime.MINUTE.ms());
         } catch (InterruptedException e) {
         }
         firm.doneWithRoom();
         for (Developer dev : developers) {
             dev.notify();
         }
-        while(firm.getTime()<FirmTime.HOUR.ms()*4){
-        	try {
-        		Thread.sleep(1000);
-        	} catch (InterruptedException e) {
-        	}
+        while (firm.getTime() < FirmTime.HOUR.ms() * 4) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
         }
         lock();
-        System.out.println(firm.getTime()+": TeamLead #"+this.id+" goes on lunch.");
-        try{
-        	Thread.sleep(FirmTime.MINUTE.ms()*(30+(int)Math.random()*30));
-        } catch(InterruptedException e){}
-        System.out.println(firm.getTime()+": TeamLead #"+this.id+" ends lunch.");
+        System.out.println(firm.getTime() + ": TeamLead #" + this.id + " goes on lunch.");
+        try {
+            Thread.sleep(FirmTime.MINUTE.ms() * (30 + (int) Math.random() * 30));
+        } catch (InterruptedException e) {
+        }
+        System.out.println(firm.getTime() + ": TeamLead #" + this.id + " ends lunch.");
         unlock();
-        while(firm.getTime()<FirmTime.HOUR.ms()*8){
-        	try {
-        		Thread.sleep(1000);
-        	} catch (InterruptedException e) {
-        	}
+        while (firm.getTime() < FirmTime.HOUR.ms() * 8) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
         }
         // Alert manager?
         firm.attemptJoin();
-        while(firm.getTime()-this.entered<8*FirmTime.HOUR.ms()){
-        	try {
-        		Thread.sleep(1000);
-        	} catch (InterruptedException e) {
-        	}
+        while (firm.getTime() - this.entered < 8 * FirmTime.HOUR.ms()) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
         }
         lock();
-        System.out.println(firm.getTime()+": TeamLead #"+this.id+" goes home.");
+        System.out.println(firm.getTime() + ": TeamLead #" + this.id + " goes home.");
     }
 
     public synchronized void unlock() {
@@ -115,14 +115,14 @@ public class TeamLead {
      */
     public synchronized void askedQuestion() {
         // 50% chance of coming up with the answer
-    	this.waitForUnlock.waitUntilMet(100);
+        this.waitForUnlock.waitUntilMet(100);
         boolean canIAnswer = Util.randomInBetween(0, 1) == 1;
-        
+
         // 10 minute wait only applies for the manager
         //Thread.sleep(FirmTime.MINUTE.ms() * 10);
         // See if we need to talk to the SPM incase we don't know the answer
         if (!canIAnswer) {
-        	this.lock();
+            this.lock();
             this.askQuestion();
             this.unlock();
         }
@@ -131,8 +131,8 @@ public class TeamLead {
 
     public synchronized void askQuestion() {
         this.lock();
-    	this.manager.askQuestion();
-    	unlock();
+        this.manager.askQuestion();
+        unlock();
     }
 
     /**
